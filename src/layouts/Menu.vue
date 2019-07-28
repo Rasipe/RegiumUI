618b6c4b73f93860532438728580dd9f460b7da1
Rafael <rafaelsilvapegoretti@gmail.com>
2019-07-28 20:11:45 -0300

    first commit

---
+++ src/layouts/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+  <q-img class="window-height" src="../assets/Regium Bandeira.png">
+    <div class="absolute-full flex flex-center">
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </div>
+  </q-img>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-page>
+    <h1>Regium</h1>
+    <div id="form">
+      <q-form class="q-gutter-md">
+        <q-input dark outlined v-model="text" label="usuario" color="yellow" type="text" />
+        <q-input dark outlined v-model="password" type="password" label="Senha" color="yellow" />
+        <q-input
+            dark
+            v-if="signUp"
+            outlined
+            v-model="confirmPassword"
+            type="password"
+            label="Confirme a Senha"
+            color="yellow"
+        />
+        <div class="row">
+          <div class="col">
+            <q-btn
+            rounded
+            @click="onSubmit()"
+            class="full-width q-mt-md"
+            label="Confirmar"
+            glossy
+            type="submit"
+            color="amber"
+            />
+          </div>
+          <div class="col">
+            <q-btn
+              rounded
+              @click="signUp = !signUp"
+              class="full-width q-mt-md"
+              glossy
+              type="submit"
+              color="amber"
+            >{{signUp ? 'Log in' : 'Sign in'}}</q-btn>
+          </div>
+        </div>
+      </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    signUp: Boolean,
+  },
+  data() {
+    return {
+      text: '',
+      password: '',
+      confirmPassword: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$router.push({ path: '/Index' });
+    },
+  },
+};
+</script>
+
+<style>
+#form {
+    background-color: rgba(0, 0, 0, 0.4);
+    border-radius: 50px;
+    padding-right: 15px;
+}
+</style>
